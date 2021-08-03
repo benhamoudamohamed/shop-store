d48061edc275d1f291fa41fe57d2bdf8c40d3cc5
Mohamed Ben Hamouda <benhamouda.mohamed@outlook.com>
2021-08-03 10:34:46 +0100

    third

--- frontend/backend/src/coupons/dto/userLimit.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Type } from "class-transformer";
-import { Min } from "class-validator";
-
-export class userLimitDto {
-
-    @Min(0, { message: 'User Limit must be a postive number' })
-    @Type(() => Number)
-    userLimit: number;
-}
